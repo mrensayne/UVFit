96d8e0ffdb39ab00d518386f4ee125bd2bdb5741
Brandon Sipos <mrensayne5@yahoo.com>
2018-11-27 15:52:00 -0700

    Fixed Polling Problem But...
    Working on changing activities to packets so I can more reliably send the data when interent cuts off and comes back on.

--- particle/UAVSensor.h
+++ particle/UAVSensor.h
@@ -51,36 +51,58 @@ void loop() {
 		digitalWrite(PowerLED, HIGH);
 		while (true)
 		{//Lets loop inside the powered on state
+			//Serial.print("."); // Doing this to see if it crashes or wtf is going on
 			input = getInput();
 			if (input == Input::TRUEPOWER)
 			{//Power button pressed when device on so power off
+				Serial.println("Powering Off");
 				break;
 			}
 			if (initUvThresh)
 			{
+				Serial.println("Grabbing Init Threshholds");
 				Particle.publish("DevSettings", "", PRIVATE);
 				initUvThresh = false;
 			}
 			if (SM.Tick(input, SensorData))
 			{ // Publish Flag Was High
+				Serial.println("Send Flag Was High From STate maching");
+			  //Setting this here because if we failed a send, we need to be able to try again the next time user manually sends data
+				responseGotten = true;
 				Activities.push_back(SensorData);
 				SensorData.clear();
+				/*if (Particle.connected())*/
 				if (Particle.connected())
 				{
+					Serial.println("Particle Connected");
 					pollUploading = false;
-					uploadData();
-					TwentyFourHours = 0;
+					if (uploadData())
+					{// If return is true then we successfully posted
+						TwentyFourHours = 0;
+					}
+					else
+					{
+						pollUploading = true;
+					}
 				}
 				else
 				{
+					Serial.println("Particle Not Connected");
 					pollUploading = true;
 					Serial.println("Particle Unable to upload");
 				}
 			}
 			if (pollUploading && SM.getState() != State::SEND)
 			{
-				if (Particle.connected() && pollcounter > 100)
+				Serial.println("Polling Uploads is HIGH");
+				if (Activities.size() == 0)
+				{//Making sure that we aren't polling when nothing is left to upload still due to late arrival of server response
+					Serial.println("Stopping polling because activities is empty");
+					pollUploading = false;
+				}
+				if (Particle.connected() && pollcounter > 1000)
 				{
+					Serial.println("Particle Connected and PollCounter is trying to upload");
 					pollcounter = 0;
 					pollUploading = false;
 					uploadData();
@@ -93,10 +115,12 @@ void loop() {
 			}
 			if (TwentyFourHours >= 864000)
 			{//24 hours has passed
+				Serial.println("Clearing All Data due to 24 hours");
 				Activities.clear();
 			}
 			if (threshcounter >= 200)
 			{// Commented out for testing, uncomment when done
+				Serial.println("Grabbing Thresh because of threshcounter");
 				Particle.publish("DevSettings", "", PRIVATE);
 				threshcounter = 0;
 			}
@@ -107,11 +131,17 @@ void loop() {
 	delay(200);
 }
 
-void uploadData()
+bool uploadData()
 {
+	Serial.println("");
+	Serial.print("Activities.size() = ");
+	Serial.print(Activities.size());
+	Serial.println("");
+	responseGotten = true;
 	int x{ 0 };
 	while (Activities.size() > 0)
 	{
+		Serial.print("|");
 		if (responseGotten)
 		{
 			Serial.println("");
@@ -121,15 +151,18 @@ void uploadData()
 			if (!postData(Activities[0]))
 			{//Unable to post because of network problem
 				Serial.println("Here is the problem");
-				return;
+				pollUploading = true;
+				return false;
 			}
 			else
 			{//Post Successful
-				Activities.erase(Activities.begin());
+				//Activities.erase(Activities.begin()); 
 				x++;
 			}
 		}
+		delay(100);
 	}
+	return true;
 }
 
 
@@ -165,7 +198,7 @@ bool postData(Data data)
 			if (DelayCounter > 20)
 			{// No Wifi Signal Found or Server Down Time Out
 				Serial.println("Postdata() Timed out");
-				Activities.push_back(data);
+				//Activities.push_back(data);
 				x = 0;
 				DelayCounter = 0;
 				return false;
@@ -180,12 +213,14 @@ bool postData(Data data)
 	}
 	x = 0;
 	DelayCounter = 0;
+	//Activities.erase(Activities.begin());
 	return true;
 }
 
 
 void myHandler(const char *event, const char *data) {
 	responseGotten = true;
+	Activities.erase(Activities.begin());
 	x += 5;
 	// Formatting output
 	String output = String::format("Response from Post:\n  %s\n", data);
@@ -234,4 +269,13 @@ void settingsHandler(const char *event, const char *data) {
 	Serial.print(atoi(uvData));
 	Serial.println("");
 	SM.setUVThreshHold(atoi(uvData));
+}
+
+void ResetWiFiConnection()
+{
+	WiFi.off();
+	delay(10);
+	WiFi.on();
+	WiFi.connect();
+	Particle.connect();
 }
