2273208eb7ba6cdf5591fbed212f491af8eb2793
Brandon Sipos <mrensayne5@yahoo.com>
2018-12-11 15:07:08 -0700

    Fixed Calorie Miscalculation

--- myapp/routes/event.js
+++ myapp/routes/event.js
@@ -2,24 +2,37 @@ var express = require('express');
 var User = require('../models/user');
 var router = express.Router();
 
+//eventtime = x, eventDuration = y, eventID = 
 router.post("/", function(req, res) {
-	var apiKeyFail = false;
+    var apiKeyFail = false;
+    var cal = 0;
 	User.findOne({ 'dev.devID': req.body.a }, function(err, user) {
 		if (err) {
 			return res.status(500).json("Error searching through DB");
 		}
 		if (user) {
-			var speedArray = JSON.parse(req.body.e);
-			var cal = speedArray[0] * req.body.y * 200 * 0.63; //assuming user weight of 200, taken from https://www.runnersworld.com/nutrition-weight-loss/a20825897/how-many-calories-are-you-really-burning-0/				
+            var speedArray = JSON.parse(req.body.e);       			
 			for (var i = 0; i < user.dev.length; i++) {
 				if (user.dev[i].devKey == req.body.apikey) {
 					//console.log(req.body);
 					if (req.body.z == "Auto") 
 					{ //no acitivty type specified
 						var act = "";
-						if (speedArray[0] < 5) act = "Walking";
-						else if (speedArray[0] < 10) act = "Running";
-						else act = "Biking";
+                        if (speedArray[0] < 5) {
+                            act = "Walking";
+                            //.3914 is in units [(calorie*second)/(meter) and speedArray is in units (meters/second)]
+                            cal = speedArray[0] * .1864; //assuming user weight of 200, taken from https://www.runnersworld.com/nutrition-weight-loss/a20825897/how-many-calories-are-you-really-burning-0/	
+                        }
+                        else if (speedArray[0] < 10) {
+                            act = "Running";
+                            //.3914 is in units [(calorie*second)/(meter) and speedArray is in units (meters/second)]
+                            cal = speedArray[0] * .3914; //assuming user weight of 200, taken from https://www.runnersworld.com/nutrition-weight-loss/a20825897/how-many-calories-are-you-really-burning-0/	
+                        }
+                        else {
+                            act = "Biking";
+                            //.3914 is in units [(calorie*second)/(meter) and speedArray is in units (meters/second)]
+                            cal = speedArray[0] * .2114; //assuming user weight of 200, taken from https://www.runnersworld.com/nutrition-weight-loss/a20825897/how-many-calories-are-you-really-burning-0/	
+                        }
 						var activ = { eventTime: req.body.x, eventDuration : req.body.y, eventID : req.body.f, deviceID : req.body.a, longitude : req.body.b, latitude : req.body.c, UV : req.body.d, speed : req.body.e, actTypeAct : act , calories : cal };
 						console.log(activ);
 						User.update({ _id: user._id }, { $push: { activities: activ }  }, function(err, user) {
